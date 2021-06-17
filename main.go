c26c7c8bd196364b356c37ab43a048839ade9cca
lyx0 <stefan@parfuss.me>
2021-06-18 01:06:43 +0200

    handle shutdown

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/bwmarrin/discordgo"
 	"github.com/gempir/go-twitch-irc/v2"
 	"github.com/lyx0/nourybot-dc/bot"
@@ -23,14 +27,31 @@ func main() {
 
 	bot := bot.NewBot(cfg, twitchClient, discordClient, sqlClient)
 
-	err = bot.ConnectTwitch()
-	if err != nil {
-		log.Fatal("Couldn't connect to Twitch", err)
-	}
+	go func() {
+		log.Info("Starting connection to Twitch")
 
-	err = bot.ConnectDiscord()
-	if err != nil {
-		log.Fatal("Couldn't connect to Discord", err)
-	}
+		err = bot.ConnectTwitch()
+		if err != nil {
+			log.Fatal("Couldn't connect to Twitch", err)
+			os.Exit(1)
+		}
+	}()
 
+	go func() {
+		log.Info("Starting connection to Discord")
+
+		err = bot.ConnectDiscord()
+		if err != nil {
+			log.Fatal("Couldn't connect to Discord", err)
+			os.Exit(1)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-c
+
+	sig := <-c
+	log.Info("Got signal:", sig)
+	os.Exit(0)
 }
